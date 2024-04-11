80de0095be6c0fad00573018629fa0d6b0075213
Thomas HEROGUELLE <totosh93270@gmail.com>
2024-04-11 10:41:24 +0200

    connexion works

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -2,9 +2,12 @@ import { Router } from "express";
 import { UserController } from "../controllers/UserController";
 
 const userController = new UserController();
-const userRouter = Router(); 
+const userRouter = Router();
 
-userRouter.post("/signup", (req , res) => { 
-    userController.signup(req, res);
+userRouter.post("/signup", (req, res) => {
+  userController.signup(req, res);
+});
+userRouter.post("/login", (req, res) => {
+  userController.login(req, res);
 });
 export default userRouter;
